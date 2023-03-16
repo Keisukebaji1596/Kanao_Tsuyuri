80ad6f168805be264963f1cd5a5e2068b574cdb4
Keisukebaji1596 <125670603+Keisukebaji1596@users.noreply.github.com>
2023-03-16 11:20:48 +0530

    Update KeysSecret.py

--- KeysSecret.py
+++ KeysSecret.py
@@ -8,24 +8,24 @@ Pass the value in C like "First Second Third" make sure you seprate them using s
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", 69))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-DB_URI = getenv("DB_URI")
-DB_NAME = getenv("DB_NAME")
-OWNER = int(getenv("OWNER_ID"))
-SUDO = list({int(i) for i in getenv("SUDO")})
-OWNER_ID = []
+API_ID = int(getenv("API_ID", 26959103))
+API_HASH = getenv("API_HASH", "ebe24f37c6f8ee727fc406c68ba5bc70")
+BOT_TOKEN = getenv("BOT_TOKEN", "6204804430:AAGoMvBeD9Fz_rJ7G4v5tYw4-CzcUgtc0rY")
+DB_URI = getenv("mongodb+srv://Anshul0554:Anshul0554@cluster0.uwx7fnj.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = getenv("luster0")
+OWNER = int(getenv())
+SUDO = list({int(i) for i in getenv(5657873637)})
+OWNER_ID = ["5657218265"]
 OWNER_ID.append(OWNER)               
 OWNER_ID.extend(SUDO)
 C = getenv("CATEGORY").split(None) # Don't remove this line
 x = []
 for i in C:
     x.append(i.strip().lower())
-COIN_NAME = str(getenv("COIN_NAME"))
-COIN_EMOJI = getenv("COIN_EMOJI")
+COIN_NAME = str(getenv("Bounty"))
+COIN_EMOJI = getenv("฿")
 CATEGORY = x
-AMOUNT = int(getenv("AMOUNT", 5))
-CHAT_ID = int(getenv("CHAT_ID"))
-PREMIUM_CHANNEL = int(getenv("PREMIUM_CHANNEL"))
-PREMIUM_COST = int(getenv("PREMIUM_COST", 50))
+AMOUNT = int(getenv("AMOUNT", 5000))
+CHAT_ID = int(getenv(-655759304))
+PREMIUM_CHANNEL = int(getenv(-655759304))
+PREMIUM_COST = int(getenv(10, 50))
